b2c165bc9a74ccdff0a6f6ea4a67755ada7938b5
Tolya Fam <famovkin@yandex.ru>
2021-12-19 20:04:54 +0300

    feat: сверстать cart

--- src/App.js
+++ src/App.js
@@ -10,10 +10,59 @@ import card4 from "./images/card-4.jpg";
 import plus from "./images/button-plus.svg";
 import search from "./images/search.svg";
 import heart from "./images/heart-default.svg";
+import remove_button from "./images/remove-button.svg";
 
 function App() {
   return (
     <div className="page">
+      <div className="cart">
+        <div className="cart__sidebar">
+          <h2 className="cart__title">Корзина</h2>
+          <ul className="cart__items">
+            <li className="cart-item">
+              <img className="cart-item__image" src={card2} alt="#"></img>
+              <div className="cart-item__text">
+                <p className="cart-item__title">
+                  Мужские Кроссовки Nike Air Max 270
+                </p>
+                <p className="cart-item__price">12 999 руб.</p>
+              </div>
+              <img
+                className="cart-item__remove-button"
+                src={remove_button}
+                alt="Крестик"
+              ></img>
+            </li>
+            <li className="cart-item">
+              <img className="cart-item__image" src={card4} alt="#"></img>
+              <div className="cart-item__text">
+                <p className="cart-item__title">
+                  Мужские Кроссовки Nike Air Max 270
+                </p>
+                <p className="cart-item__price">8 499 руб.</p>
+              </div>
+              <img
+                className="cart-item__remove-button"
+                src={remove_button}
+                alt="Крестик"
+              ></img>
+            </li>
+          </ul>
+          <ul className="order-info">
+            <li className="order-info__content">
+              <p className="order-info__title">Итого:</p>
+              <div className="order-info__dots"></div>
+              <p className="order-info__value">21 498 руб.</p>
+            </li>
+            <li className="order-info__content">
+              <p className="order-info__title">Налог 5%:</p>
+              <div className="order-info__dots"></div>
+              <p className="order-info__value">1074 руб.</p>
+            </li>
+          </ul>
+          <button className="cart__submit-button">Оформить заказ</button>
+        </div>
+      </div>
       <div className="page__wrapper">
         <header className="header">
           <div className="header__content">
